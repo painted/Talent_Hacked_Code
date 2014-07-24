1a74958aa97b595b7a432c89101f7589ad7b11de
Painted <will.allen@paintedltd.co.uk>
2014-07-24 14:56:37 +0100

    edited the client profile show page. now with tests

--- spec/features/client_profile_view_spec.rb
+++ spec/features/client_profile_view_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-describe 'developer profile view' do
+describe 'client profile view' do
 
-	context 'developer' do
+	context 'client' do
 
 		before do
-			john = Developer.create(email: 'f@f.com', password: '12345678', password_confirmation: '12345678')
-			login_as john
-			visit edit_developer_registration_path
+			peter = Client.create(email: 'f@f.com', password: '12345678', password_confirmation: '12345678')
+			login_as peter, scope: :client
+			visit edit_client_registration_path
 			fill_in "Name", with: "John"
 			fill_in "Email", with: "a@a.com"
 			fill_in "Phone", with: "07777 777 777"
-			fill_in "Date of Birth", with: "01/01/1980"
+			fill_in "Industry", with: "Military"
 			fill_in "Address", with: "25 City Road, London"
 			fill_in "Website", with: "https://www.google.co.uk"
 			fill_in "Facebook", with: "https://www.facebook_url"
@@ -22,47 +22,47 @@ describe 'developer profile view' do
 			click_button "Update"
 		end
 
-		it 'can welcome the developer by name' do
+		it 'can welcome the client by name' do
 			expect(page).to have_content 'Profile for John'
 		end
 
-		it 'can show the developer his email' do
+		it 'can show the client his email' do
 			expect(page).to have_content 'a@a.com'
 		end
 
-		it 'can show the developer his phone number' do
+		it 'can show the client his phone number' do
 			expect(page).to have_content '07777 777 777'
 		end
 
-		it 'can show the developer his date of birth' do
-			expect(page).to have_content '1980-01-01'
+		it 'can show the client his date of industry' do
+			expect(page).to have_content 'Military'
 		end
 
-		it 'can show the developer his address' do
+		it 'can show the client his address' do
 			expect(page).to have_content '25 City Road, London'
 		end
 
-		it 'can show the developer his website' do
+		it 'can show the client his website' do
 			expect(page).to have_content 'https://www.google.co.uk'
 		end
 
-		it 'can show the developer his facebook' do
+		it 'can show the client his facebook' do
 			expect(page).to have_content 'https://www.facebook_url'
 		end
 
-		it 'can show the developer his twitter' do
+		it 'can show the client his twitter' do
 			expect(page).to have_content 'https://www.twitter_url'
 		end
 
-		it 'can show the developer his github url' do
+		it 'can show the client his github url' do
 			expect(page).to have_content 'https://www.github_url'
 		end
 
-		it 'can show the developer his LinkedIn url' do
+		it 'can show the client his LinkedIn url' do
 			expect(page).to have_content 'https://www.linkedin_url'
 		end
 
-		it 'can show the developer his avatar url' do
+		it 'can show the client his avatar url' do
 			expect(page).to have_content 'https://www.avatar_url'
 		end
 
@@ -72,7 +72,7 @@ describe 'developer profile view' do
 
 		it 'goes to the edit profile page with edit button click' do 
 			click_link "Edit"
-			expect(current_path).to include edit_developer_registration_path
+			expect(current_path).to include edit_client_registration_path
 		end
 	end	
 end
