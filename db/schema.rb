8b771896f4aa2ebddb568b47c421f6aaad5887d9
Painted <will.allen@paintedltd.co.uk>
2014-07-26 16:05:08 +0100

    Added Company Bio to Clients.  Please do a migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140726143002) do
+ActiveRecord::Schema.define(version: 20140726150144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20140726143002) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.text     "companyBio"
   end
 
   add_index "clients", ["email"], name: "index_clients_on_email", unique: true, using: :btree
