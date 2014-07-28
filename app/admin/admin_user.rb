bdd91986e7a0ab23bf9419ae228bc6f93601b1bd
Nadav <nadav.matalon@gmail.com>
2014-07-28 20:18:30 +0100

    refactoring_active_admin

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,4 +1,7 @@
 ActiveAdmin.register AdminUser do
+
+  menu priority: 4, label: "Admins"
+
   permit_params :email, :password, :password_confirmation
 
   index do
--- app/admin/client.rb
+++ app/admin/client.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register Client do
 
-  
+  menu priority: 2
+
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
