4083230254c5d42081ec92bf81d3f53ed761345c
Painted <will.allen@paintedltd.co.uk>
2014-07-28 15:07:52 +0100

    developer verification functionality by admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140727172508) do
+ActiveRecord::Schema.define(version: 20140728135355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,12 +101,12 @@ ActiveRecord::Schema.define(version: 20140727172508) do
   add_index "contacts", ["client_id"], name: "index_contacts_on_client_id", using: :btree
 
   create_table "developers", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -131,6 +131,7 @@ ActiveRecord::Schema.define(version: 20140727172508) do
     t.datetime "image_updated_at"
     t.integer  "dayrate"
     t.text     "bio"
+    t.boolean  "verified",               default: false, null: false
   end
 
   add_index "developers", ["email"], name: "index_developers_on_email", unique: true, using: :btree
