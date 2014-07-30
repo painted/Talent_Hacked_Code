7e8c17eea3d135a17103786d62a660019cd149f9
Painted <will.allen@paintedltd.co.uk>
2014-07-30 09:56:52 +0100

    added viable developers method in projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,10 @@ class Project < ActiveRecord::Base
   has_many :levels
   has_many :proficiencies
   has_many :statuses
+
+	def viable_developers
+		Developer.where(verified: true)(.to_a).select do | developer|
+	    	skills.all? {| project_skill | developer.skills.include?(project_skill) }
+	    end
+	end
 end
